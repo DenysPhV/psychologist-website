665306c5f501df746cd48f897118e740fa5383e9
Denys Filichkin <denys.philichkin@gmail.com>
2023-11-01 18:05:49 +0200

    fix photo component

--- src/components/photo.js
+++ src/components/photo.js
@@ -1,13 +1,23 @@
 /** @format */
-
-import React from "react";
 import "../images/photo_ps.jpg";
 import "../styles/photo.css";
 
-function Photo({ photo, onPhotoChange }) {
+import React, { useEffect, useState } from "react";
+
+function Photo({ onPhotoChange }) {
+  // Спробуємо отримати фото з локального сховища при завантаженні сторінки
+  const storedPhoto = localStorage.getItem("psychologistPhoto");
+  const [photo, setPhoto] = useState(storedPhoto);
+
   // Функція для обробки зміни фото
   const handlePhotoUpload = (event) => {
     const newPhoto = URL.createObjectURL(event.target.files[0]);
+    setPhoto(newPhoto);
+
+    // Зберігаємо нове фото в локальному сховищі
+    localStorage.setItem("psychologistPhoto", newPhoto);
+
+    // Викликаємо функцію батьківського компонента для збереження фото
     onPhotoChange(newPhoto);
   };
 
