83f5a313617c66242555b98b7423f135866a399e
Denys Filichkin <denys.philichkin@gmail.com>
2023-11-02 16:12:27 +0200

    save between

--- src/components/services.js
+++ src/components/services.js
@@ -2,11 +2,13 @@
 
 import React from "react";
 
+import "../styles/services.css";
+
 function Services({ servicesData }) {
   return (
-    <div className="services">
-      <h2>Послуги психолога</h2>
-      <ul>
+    <div className="services-container">
+      <h2 className="services-title">Послуги психолога</h2>
+      <ul className="services-list">
         {servicesData.map((service, index) => (
           <li key={index}>{service}</li>
         ))}
